391e14b0c8c0e62a12e31c476f769878605b9098
ManuMalempati <137015324+ManuMalempati@users.noreply.github.com>
2024-11-29 12:39:12 +1100

    Final touches
    Removed some debug statements, added comments and finalised game logic.

--- masterfile.c
+++ masterfile.c
@@ -9,13 +9,16 @@ void add_matrices(double **matrix1, double **matrix2, int rows, int columns);
 int strcmp(char s1[], char s2[]);
 
 int main(int argc, char** argv) {
+
     char input_filename[1024];
     char answer[1024];
+
     printf("Please enter the filename of the image you want to guess (Max 1024 characters): ");
     scanf("%s", input_filename);
     printf("Please enter the name of the image (Max 1024 characters): ");
     scanf("%s", answer);
     printf("Loading %s...\n", input_filename);
+    
     const char *output_filename = "output.bmp";
     int width, height, hints;
 
@@ -26,18 +29,11 @@ int main(int argc, char** argv) {
 
     printf("How many hints?: ");
     scanf("%d", &hints);
+    hints++; // since the first image is given and not considered a hint.
 
     if (matrix) {
         printf("Height: %d\n", height);
         printf("Width: %d\n", width);
-
-        // Print some grayscale values for debugging
-        for (int i = 0; i < height && i < 10; i++) {
-            for (int j = 0; j < width && j < 10; j++) {
-                printf("%f ", matrix[i][j]);
-            }
-            printf("\n");
-        }
 
         // Now we adjust matrix for one based indexing
         double **a = dmatrix(1, height, 1, width);
@@ -93,13 +89,17 @@ int main(int argc, char** argv) {
         }
 
         char filename[100];
+        int hints_used=0;
 
         printf("Please give the device to the other user to guess.\n");
         
         for(int i=0; i < hints; i++){
-            // pick counter-counter+increment_by rows from U_reduced, values from W_reduced and columns from V_T
+            // pick counter-counter+increment_by rows from U_reduced, values from W_reduced and columns from V_T (next set of elements)
             
             for(int j=counter; j<counter+increment_by && j<= singular_values; j++){
+
+                // store column vector and row vector corresponding to the singular values from the decomposition
+
                 double *U_slice = (double*)calloc(height, sizeof(double));
                 double *V_slice = (double*)calloc(width, sizeof(double));
                 if(U_slice == NULL || V_slice == NULL){
@@ -113,33 +113,43 @@ int main(int argc, char** argv) {
                     V_slice[k-1] = V_T[j][k];
                 }
 
+                // Multiply the row & column vectors and scalar to generate matrix.
                 double **temp_result = column_row_scalar_multiply(U_slice, V_slice, height, width, singular_value);
+
+                // Accumulating detail to the image through the addition of a set number of matrices generated via the singular values
                 add_matrices(result, temp_result, height, width);
                 
                 free(U_slice);
                 free(V_slice);
                 free_dmatrix(temp_result, 1, height, 1, width);
             }
+
             counter = counter + increment_by;
+
+            // Some svd values have been used to generate another image so the user has used another hint.
+            hints_used++;
             sprintf(filename, "output_hint_%d.bmp", i+1);
             write_grayscale_bmp(filename, result, width, height);
             char guess[100];
-            printf("%s has been generated.\nType your guess (type 'h' to use the next hint, type '0' to exit): ", filename);
+            printf("%s has been generated.\nType your guess (type 'h' to use the next hint (%d hints remaining), type '0' to exit): ", filename, hints-hints_used);
             scanf("%s", guess);
-            while((strcmp(guess, answer) != 0) && (strcmp(guess, "h") != 0 )){
+            while((strcmp(guess, answer) != 0) && (strcmp(guess, "h") != 0 ) && (strcmp(guess, "0") != 0)){
                 printf("Incorrect.\n");
-                printf("Type your guess (type 'h' to use the next hint, type '0' to exit): ");
+                printf("Type your guess (type 'h' to use the next hint (%d hints remaining), type '0' to exit): ", hints-hints_used);
                 scanf("%s", guess);
             }
             if(strcmp(guess, answer) == 0){
                 printf("Correct! You guessed the answer with %d hint(s).\n", i);
-                // FIX THIS, YOU HAVENT FREED ALL THE MEMORY YET
-                return 0;
+                break;
             }
-
+            else if (strcmp(guess, "0") == 0){
+                printf("Exiting program.\n");
+                break;
+            }
+            // If the user enters 'h' then the loop will continue and generate new images.
         }
 
-        //generates a greyscale image using the extracted pixel values.
+        //generates a greyscale version of the original image using the extracted pixel values.
         write_grayscale_bmp(output_filename, matrix, width, height);
         free_dmatrix(result, 1, height, 1, width);
         free_matrix(matrix, height);
@@ -148,7 +158,10 @@ int main(int argc, char** argv) {
         free_dmatrix(V_reduced, 1, width, 1, width);
         free_dmatrix(V_T, 1, width, 1, width);
         free_dmatrix(a, 1, height, 1, width);
-        //printf("You have failed to guess the correct answer :(\n");
+        if(hints == hints_used){
+            printf("All hints have been used.\n");
+        }
+        printf("The answer was: %s.\n", answer);
         printf("Done.\n");
 
         
@@ -159,6 +172,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+// Helper function for comparing 2 strings of any size
 int strcmp(char s1[], char s2[]){
     int counter=0;
     while(1){
@@ -204,9 +218,6 @@ void add_matrices(double **matrix1, double **matrix2, int rows, int columns) {
         }
     }
 }
-
-
-
 
 // gcc -Wall -o masterfile.exe masterfile.c svdcmp.c decomposer.c image_utils.c -lm
 // .\masterfile
